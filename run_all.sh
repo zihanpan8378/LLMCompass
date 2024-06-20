71089591a7cb022311b24012dd9f63fa70c2254c
Zihan Pan <z82pan@uwaterloo.ca>
2024-06-19 21:07:07 -0400

    Add script to run all experiments

---
+++ run_all.sh
@@ -0,0 +1,41 @@
+#!/bin/bash
+
+# # Figure 5
+# cd ae/figure5
+# bash run_figure5.sh
+# cd ../..
+
+# # Figure 6
+# cd ae/figure6
+# bash run_figure6.sh
+# cd ../..
+
+# Figure 7
+cd ae/figure7
+bash run_figure7.sh
+cd ../..
+
+# Figure 8
+cd ae/figure8
+bash run_figure8.sh
+cd ../..
+
+# Figure 9
+cd ae/figure9
+bash run_figure9.sh
+cd ../..
+
+# Figure 10
+cd ae/figure10
+bash run_figure10.sh
+cd ../..
+
+# Figure 11
+cd ae/figure11
+bash run_figure11.sh
+cd ../..
+
+# Figure 12
+cd ae/figure12
+bash run_figure12.sh
+cd ../..
