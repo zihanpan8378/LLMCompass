2c2f349fa798ac58b72c333e040b0bcd629ac779
Zihan Pan <z82pan@uwaterloo.ca>
2024-06-26 15:56:46 -0400

    Update softmax test script

---
+++ ae/energy_experiments/run_softmax.sh
@@ -0,0 +1,6 @@
+cd ../..
+
+python -m ae.energy_experiments.test_softmax --simgpu
+
+#cd ae/figure5/cf
+#python plot_softmax.py
--- ae/energy_experiments/test_softmax.py
+++ ae/energy_experiments/test_softmax.py
@@ -1,7 +1,9 @@
 from software_model.softmax import Softmax
 from software_model.utils import data_type_dict, Tensor
 from hardware_model.device import device_dict
+from design_space_exploration.dse import template_to_system, read_architecture_template
 import argparse
+import os
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
@@ -10,7 +12,19 @@ if __name__ == "__main__":
     args = parser.parse_args()
 
     A100 = device_dict["A100_80GB_fp16"]
-    gpu_overhead = 12e-6
+    RTX4090 = template_to_system(read_architecture_template("configs/RTX4090.json")).device
+    gpu_overhead = 12.1e-6
+    
+    device = RTX4090
+    
+    device_name = "RTX4090"
+    print(f"Device: {device_name} {device}")
+    
+    file_name=f'ae/energy_experiments/matmul_{device_name}_sim.csv'
+    if os.path.exists(file_name):
+        os.remove(file_name)
+        with open(file_name, 'w') as f:
+            f.write('')
 
     if args.gpu:
         gpu_kernel_launch_overhead = Softmax.gpu_kernel_launch_overhead()
@@ -26,17 +40,13 @@ if __name__ == "__main__":
         if args.gpu:
             latency = model.run_on_gpu()
         if args.simgpu:
-            result = model.compile_and_simulate(pcb_module=A100)
+            result = model.compile_and_simulate(pcb_module=device)
             latency = result[0] + gpu_overhead
             energy = result[1]
-            file_name = "softmax_A100_sim"
             
         print(f"Performance: {M}, {N}, {M*N/latency/1e9}, {energy}")
-        with open(f"ae/energy_experiments/{file_name}_perf.csv", "a") as f:
-            f.write(f"{M}, {N}, {M*N/latency/1e9}\n")
-            
-        with open(f"ae/energy_experiments/{file_name}_energy.csv", "a") as f:
-            f.write(f"{M}, {N}, {energy}\n")
+        with open(file_name, "a") as f:
+            f.write(f"{M}, {N}, {M*N/latency/1e9} {energy}\n")
 
     N = 2**12
     for M in range(5, 16):
@@ -48,13 +58,10 @@ if __name__ == "__main__":
         if args.gpu:
             latency = model.run_on_gpu()
         if args.simgpu:
-            result = model.compile_and_simulate(pcb_module=A100)
+            result = model.compile_and_simulate(pcb_module=device)
             latency = result[0] + gpu_overhead
             energy = result[1]
         
         print(f"Performance: {M}, {N}, {M*N/latency/1e9}, {energy}")
-        with open(f"ae/energy_experiments/{file_name}_perf.csv", "a") as f:
-            f.write(f"{M}, {N}, {M*N/latency/1e9}\n")
-            
-        with open(f"ae/energy_experiments/{file_name}_energy.csv", "a") as f:
-            f.write(f"{M}, {N}, {energy}\n")
+        with open(file_name, "a") as f:
+            f.write(f"{M}, {N}, {M*N/latency/1e9} {energy}\n")
