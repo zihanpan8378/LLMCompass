d5e95707de750ccb1e7908b8626fa00b68bfcc44
Zihan Pan <z82pan@uwaterloo.ca>
2024-07-02 18:01:19 -0400

    Update matmul test script

--- ae/energy_experiments/run_matmul.sh
+++ ae/energy_experiments/run_matmul.sh
@@ -1,7 +1,8 @@
 cd ../..
 
-python -m ae.energy_experiments.test_matmul --mode=sim --device=A100
+# python -m ae.energy_experiments.test_matmul --mode=sim --device=A100
 python -m ae.energy_experiments.test_matmul --mode=sim --device=RTX4090
+# python -m ae.energy_experiments.test_matmul --mode=run --device=RTX4090
 
 #cd ae/figure5/ab
 #python plot_matmul.py
