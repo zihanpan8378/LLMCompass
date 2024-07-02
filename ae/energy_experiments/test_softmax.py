9056eb0b363679ea6dc7a457cd46153560494abb
Zihan Pan <z82pan@uwaterloo.ca>
2024-07-02 18:01:35 -0400

    Update softmat test script

--- ae/energy_experiments/test_softmax.py
+++ ae/energy_experiments/test_softmax.py
@@ -11,16 +11,16 @@ if __name__ == "__main__":
     parser.add_argument("--simgpu", action="store_true", help="Enable simulation")
     args = parser.parse_args()
 
-    A100 = device_dict["A100_80GB_fp16"]
+    A100 = template_to_system(read_architecture_template("configs/GA100.json")).device # device_dict["A100_80GB_fp16"]
     RTX4090 = template_to_system(read_architecture_template("configs/RTX4090.json")).device
     gpu_overhead = 12.1e-6
     
-    device = RTX4090
+    device = A100
     
-    device_name = "RTX4090"
+    device_name = "A100"
     print(f"Device: {device_name} {device}")
     
-    file_name=f'ae/energy_experiments/matmul_{device_name}_sim.csv'
+    file_name=f'ae/energy_experiments/softmax_{device_name}_sim.csv'
     if os.path.exists(file_name):
         os.remove(file_name)
         with open(file_name, 'w') as f:
